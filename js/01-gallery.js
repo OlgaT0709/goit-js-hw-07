d4cb5e978391a518b9e7fbbcc4a5e1a6a250e22c
Olga Tolstykhina <ovt0709@gmail.com>
2023-02-19 22:07:54 +0200

    createGalary and onPreviewClickRecievedImg

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.insertAdjacentHTML('beforeend', createGalary(galleryItems))
+galleryContainer.addEventListener('click', onPreviewClick);
+
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+function createGalary(items) {
+    return items.map(({ preview, original, description }) => {
+        return `
+            <div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                    />
+                </a>
+            </div>
+            `;
+    }).join('');
+};
+
+// Реалізація делегування на div.gallery і отримання url великого зображення.
+// Зверни увагу на те, що зображення обгорнуте посиланням, отже по кліку за замовчуванням користувач буде перенаправлений на іншу сторінку. Заборони цю поведінку за замовчуванням.
+function onPreviewClick(event) {
+    event.preventDefault();
+   
+    if (!event.target === galleryContainer.querySelector('.gallery__image')) {
+        return;
+    };
+
+    console.log (event.target.dataset.source); 
+        
+};
+
+// Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
+// Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
+// Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
+
+
+// Додай закриття модального вікна після натискання клавіші Escape. Зроби так, щоб прослуховування клавіатури було тільки доти, доки відкрите модальне вікно. Бібліотека basicLightbox містить метод для програмного закриття модального вікна.
